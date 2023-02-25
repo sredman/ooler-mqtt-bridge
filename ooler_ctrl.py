49b7927448b62c3b53baa45e6119da9a09022dcc
Simon Redman <simon@ergotech.com>
2023-02-25 13:10:04 -0500

    Empty CLI program to control the ooler

---
+++ ooler_ctrl.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+## Filename: ooler_ctrl.py
+## Description: Interactive tool to issue BLE commands to control the Ooler bed cooler.
+
+def main():
+    pass
+
+if __name__ == "main":
+    main()
